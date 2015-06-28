ec19c103e867295086ce77545b6bda297590c263
Sunny Lam <sunny.lam@gmail.com>
2015-06-28 17:24:57 -0400

    Setup the email service.  This is right before I alter the controller to use this service.

---
+++ js/services/emails.js
@@ -0,0 +1,17 @@
+/* 
+* using data from: 
+* https://s3.amazonaws.com/codecademy-content/courses/ltp4/emails-api/emails.json
+*
+* you create a new service called "emails" and sub in the json link
+* in the controller you use the service to fetch the emails
+*/
+
+app.factory('emails', ['$http', function($http) { 
+  return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/emails-api/emails.json') 
+            .success(function(data) { 
+              return data; 
+            }) 
+            .error(function(err) { 
+              return err; 
+            }); 
+}]);
